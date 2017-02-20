315e8d1d0c8f48cde48deb5636dcfcedd4181ec9
Roger G. Coscojuela <rogilico@gmail.com>
2017-02-20 02:27:42 +0100

    Grandes cambios en el proyecto.
    Falta acabar la parte que pinta las lineas (entre fechas)
    Falta tambien que la primera activity pase una fecha bien formateada.

--- app/src/main/java/net/blusoft/appautobusesbackoffice/MainActivity.java
+++ app/src/main/java/net/blusoft/appautobusesbackoffice/MainActivity.java
@@ -2,33 +2,44 @@ package net.blusoft.appautobusesbackoffice;
 
 import android.app.DatePickerDialog;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.text.InputType;
-import android.view.Menu;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
-import android.widget.EditText;
-import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    private EditText editTextMatricula;
+
     private int opcion = MapsActivity.OPCIO_ULTIMA;
     RadioGroup radioGroup;
     TextView editTextHInicio;
     TextView editTextHFinal;
     private DatePickerDialog fromDatePickerDialog;
     private DatePickerDialog toDatePickerDialog;
-
+    Spinner spinnerMatricules;
     private SimpleDateFormat dateFormatter;
+    private ArrayList<String> matricules;
+    private boolean spinnerCarregat = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,7 +47,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         Button btnEnviar = (Button) findViewById(R.id.buttonEnviar);
         btnEnviar.setOnClickListener(this);
-        editTextMatricula = (EditText) findViewById(R.id.editTextMatricula);
+        spinnerMatricules = (Spinner) findViewById(R.id.spinnerMatricules);
         radioGroup = (RadioGroup) findViewById(R.id.rdgGrupo);
         editTextHInicio = (TextView) findViewById(R.id.editTextHInicio);
         editTextHFinal = (TextView) findViewById(R.id.editTextHFin);
@@ -50,6 +61,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         //visibilidad
         editTextHInicio.setVisibility(View.INVISIBLE);
         editTextHFinal.setVisibility(View.INVISIBLE);
+        new populateSpinner().execute();
     }
 
     @Override
@@ -64,19 +76,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         System.out.println("date fin vale " + editTextHFinal.getText().toString());
 
         if (R.id.buttonEnviar == view.getId()) {
-            if (editTextMatricula.getText().toString().equals("")) {
-                System.out.println(editTextMatricula.getText().toString());
-                Toast.makeText(this, "Maricula Obligatoria", Toast.LENGTH_SHORT).show();
-            } else if (editTextHInicio.getText().toString().equals("") && editTextHFinal.getText().toString().equals("") && opcion == MapsActivity.OPCIO_ENTRE_DADES) {
-                Toast.makeText(this, "Fechas Obligatorias", Toast.LENGTH_SHORT).show();
+            if (spinnerCarregat) {
+                if (editTextHInicio.getText().toString().equals("") && editTextHFinal.getText().toString().equals("") && opcion == MapsActivity.OPCIO_ENTRE_DADES) {
+                    Toast.makeText(this, "Fechas Obligatorias", Toast.LENGTH_SHORT).show();
+                } else {
+                    System.out.println("opcion marcada: " + opcion);
+                    Intent i = new Intent(this, MapsActivity.class);
+                    i.putExtra("matricula", spinnerMatricules.getSelectedItem().toString());
+                    i.putExtra("opcion", opcion);
+                    i.putExtra("fechaInicio", editTextHInicio.getText().toString());
+                    i.putExtra("fechaFinal", editTextHFinal.getText().toString());
+                    startActivity(i);
+                }
             } else {
-                System.out.println("opcion marcada: " + opcion);
-                Intent i = new Intent(this, MapsActivity.class);
-                i.putExtra("matricula", editTextMatricula.toString());
-                i.putExtra("opcion", opcion);
-                i.putExtra("fechaInicio", editTextHInicio.toString());
-                i.putExtra("fechaFinal", editTextHFinal.toString());
-                startActivity(i);
+                Toast.makeText(MainActivity.this, "No s'han pogut carregar les dades. Hi ha conexió a internet?", Toast.LENGTH_LONG).show();
             }
         }
     }
@@ -121,4 +134,59 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         }, newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
     }
+
+
+    private class populateSpinner extends AsyncTask<Void, Void, Boolean> {
+
+        @Override
+        protected Boolean doInBackground(Void... params) {
+            boolean result = true;
+            JSONObject jsonobject;
+            JSONArray jsonarray;
+            matricules = new ArrayList<>();
+            try {
+                String direccioServidor = "http://server.blusoft.net:8080";
+                URL url = new URL(direccioServidor + "/ServicioWeb/webresources/generic/autobuses/");
+                BufferedReader reader = getBufferedReader(url);
+                jsonarray = new JSONArray(reader.readLine());
+                for (int i = 0; i < jsonarray.length(); i++) {
+                    jsonobject = jsonarray.getJSONObject(i);
+                    matricules.add(jsonobject.optString("matricula"));
+                }
+                matricules.add("Todas");
+            } catch (Exception e) {
+                result = false;
+                Log.e("Error", e.getMessage());
+                e.printStackTrace();
+            }
+            return result;
+        }
+
+        private BufferedReader getBufferedReader(URL url) throws java.io.IOException {
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setReadTimeout(5000 /*milliseconds*/);
+            conn.setConnectTimeout(5000);
+            conn.setRequestProperty("Content-Type", "application/json");
+            return new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        }
+
+        @Override
+        protected void onPostExecute(Boolean results) {
+            // Locate the spinner in activity_main.xml
+            System.out.println("onpostexecute");
+            if (results) {
+                Spinner mySpinner = (Spinner) findViewById(R.id.spinnerMatricules);
+                // Spinner adapter
+                mySpinner
+                        .setAdapter(new ArrayAdapter<>(MainActivity.this,
+                                android.R.layout.simple_spinner_dropdown_item,
+                                matricules));
+                spinnerCarregat = true;
+            } else {
+                Toast.makeText(MainActivity.this, "No s'han pogut carregar les dades. Hi ha conexió a internet?", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
 }
--- app/src/main/java/net/blusoft/appautobusesbackoffice/MapsActivity.java
+++ app/src/main/java/net/blusoft/appautobusesbackoffice/MapsActivity.java
@@ -1,102 +1,117 @@
 package net.blusoft.appautobusesbackoffice;
 
 import android.content.Intent;
-import android.location.Location;
+import android.graphics.Color;
 import android.os.AsyncTask;
+import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.app.FragmentActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.Random;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, View.OnClickListener {
 
-    private GoogleMap mMap;
-    String matricula_Autobus = null;
-    int opcioEscollida;
-    static final int OPCIO_ULTIMA = 1;
-    static final int OPCIO_ENTRE_DADES = 2;
+
+    public static final int OPCIO_ULTIMA = 1;
+    public static final int OPCIO_ENTRE_DADES = 2;
     private static final String LOGTAG = "BackOfficeAutobuses Map";
-    boolean actiu= true;
+    private boolean actiu=true;
+    private boolean firstUpdate=true;
+    private boolean actualitzacioAutomatica = true;
+    private GoogleMap mMap;
+    private String matricula_Autobus, dataInici, dataFi = null;
+    private int opcioEscollida=OPCIO_ULTIMA;
+    JSONObject newPosicio = null;
+    JSONObject posicio = null;
+    ArrayList marcadors = new ArrayList<>();
+    JSONArray llistanovesPosicions;
+    private String direccioServidor="http://server.blusoft.net:8080";
 
-
-    Location posicio;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        //Obtenim l'intent i les dades necesaries.
-        Button boto = (Button) findViewById(R.id.btnOpciones);
-        boto.setOnClickListener(this);
+        Button botoEnrere = (Button) findViewById(R.id.btnOpciones);
+        botoEnrere.setOnClickListener(this);
+        Button botoActAuto = (Button) findViewById(R.id.btnAturarActualitzacioAutomatica);
+        botoActAuto.setOnClickListener(this);
         if (savedInstanceState == null) {
             Intent extras = getIntent();
+            opcioEscollida = extras.getIntExtra("opcion", OPCIO_ULTIMA);
             matricula_Autobus = extras.getStringExtra("matricula");
-            opcioEscollida = extras.getIntExtra("opcion", 0);
+            switch (opcioEscollida) {
+                case OPCIO_ULTIMA:
+                    break;
+                case OPCIO_ENTRE_DADES:
+                    dataInici = extras.getStringExtra("fechaInicio");
+                    dataFi = extras.getStringExtra("fechaFinal");
+                    break;
+            }
         }
-        if (matricula_Autobus != null) {
-            //TODO DO things...
+    }
 
-            posicio = new Location("");//provider name is unecessary
-            posicio.setLatitude(41.483945);//your coords of course
-            posicio.setLongitude(2.177165);
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(LOGTAG, "Onpause...");
+        actiu=false;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(LOGTAG, "OnResume...");
+        if(actualitzacioAutomatica){
+            actiu=true;
+            Log.d(LOGTAG, "Activat");
         }
-
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         mMap.getUiSettings().setMapToolbarEnabled(false);
-        LatLng posAutobus = new LatLng(posicio.getLatitude(), posicio.getLongitude());
-        mMap.addMarker(new MarkerOptions()
-                .position(posAutobus)
-                .title(matricula_Autobus));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(posAutobus, 10));
+        Log.d(LOGTAG, "Map is ready");
+        actualitzaDades();
+    }
 
+    private void actualitzaDades(){
+        Log.d(LOGTAG, "Metodo actualitzaDades. Actiu:"+actiu+" Actualitzacio Automatica:"+actualitzacioAutomatica);
         final Handler handler = new Handler();
         final Runnable r = new Runnable() {
             public void run() {
-                AsyncTask tarea = new TareaObtenerUltimaPosicion();
-                tarea.execute();
-                Random rand = new Random();
-                /*LatLng posAutobus = new LatLng(posicio.getLatitude() + rand.nextFloat() / 1000, posicio.getLongitude() + rand.nextFloat() / 1000);
-                mMap.addMarker(new MarkerOptions()
-                        .position(posAutobus)
-                        .title(matricula_Autobus));
-                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(posAutobus, 15));*/
-                if (actiu) {
+                if (actiu && actualitzacioAutomatica) {
+                    AsyncTask tarea = new refrescarPosiciones();
+                    final AsyncTask execute = tarea.execute();
                     handler.postDelayed(this, 3000);
                 }
             }
@@ -104,73 +119,254 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         handler.postDelayed(r, 3000);
     }
 
-
+    private void desactivarActualitzacionsAutomatiques(){
+        Button boto= (Button) findViewById(R.id.btnAturarActualitzacioAutomatica);
+        boto.setText(R.string.activaActAut);
+        Log.d(LOGTAG, "Actualitzacio automatica desactivada");
+        actualitzacioAutomatica=false;
+    }
     @Override
     public void onClick(View view) {
+        if (R.id.btnAturarActualitzacioAutomatica == view.getId()) {
+            Button boto= (Button) findViewById(R.id.btnAturarActualitzacioAutomatica);
+            if(actualitzacioAutomatica){
+               desactivarActualitzacionsAutomatiques();
+            }
+            else{
+                boto.setText(R.string.atura_act_aut);
+                Log.d(LOGTAG, "Actualitzacio automatica activada");
+                actiu=true;
+                actualitzacioAutomatica=!actualitzacioAutomatica;
+                actualitzaDades();
+            }
+        }
+
         if (R.id.btnOpciones == view.getId()) {
+            Log.d(LOGTAG, "Retornant a pantalla principal.");
             Intent i = new Intent(this, MainActivity.class);
             startActivity(i);
-            actiu=false;
+            actiu = false;
             finish();
         }
     }
 
-    class TareaObtenerUltimaPosicion extends AsyncTask<Object, Integer, Boolean> {
-        JSONObject posicio;
+    class refrescarPosiciones extends AsyncTask<Object, Integer, Boolean> {
+
+
+        public static final float HUE_RED = 0.0F;
+        public static final float HUE_ORANGE = 30.0F;
+        public static final float HUE_YELLOW = 60.0F;
+        public static final float HUE_GREEN = 120.0F;
+        public static final float HUE_CYAN = 180.0F;
+        public static final float HUE_AZURE = 210.0F;
+        public static final float HUE_BLUE = 240.0F;
+        public static final float HUE_VIOLET = 270.0F;
+        public static final float HUE_MAGENTA = 300.0F;
+
 
         @Override
         protected Boolean doInBackground(Object... params) {
-            if(opcioEscollida==MapsActivity.OPCIO_ULTIMA){
-                pintarUltimaPosicion(matricula_Autobus);
+            Log.d(LOGTAG, "Do in background....");
+            switch (opcioEscollida) {
+                case MapsActivity.OPCIO_ULTIMA:
+                    ultimaPosicion(matricula_Autobus);
+                    break;
+                case MapsActivity.OPCIO_ENTRE_DADES:
+                    entreFechas();
             }
             return true;
         }
 
-       // private void pintarEntrePosiciones()
-        private void pintarUltimaPosicion(String matricula){
-            BufferedReader reader;
-            try {
-                URL url = new URL("http://192.168.50.28:8080/ServicioWeb/webresources/generic/obtenerUltimaPosicion/" + matricula);
-                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                conn.setRequestMethod("GET");
-                conn.setReadTimeout(1000 /*milliseconds*/);
-                conn.setConnectTimeout(5000);
-                conn.setRequestProperty("Content-Type", "application/json");
-                reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-                StringBuffer buffer = new StringBuffer();
-                String line = reader.readLine();
-                System.out.println(line);
-                posicio = new JSONObject(line);
-                System.out.println(posicio.toString());
-                System.out.println(line);
+        private void entreFechas() {
+            System.out.println("TODO ENTRE FECHAS");
+        }
 
-            } catch (java.io.IOException ex) {
-                Log.e(LOGTAG, "Temps d'espera esgotat al iniciar la conexio amb la BBDD extena");
+        private void ultimaPosicion(String matricula) {
+            BufferedReader reader;
+            URL url = null;
+            try {
+                if (matricula.equals("Todas")) {
+                    Log.d(LOGTAG, "Obteniendo las ultimas posiciones de todos los autobuses");
+                    url = new URL(direccioServidor+"/ServicioWeb/webresources/generic/obtenerUltimasPosicionesTodos/");
+                    reader = getBufferedReader(url);
+                    llistanovesPosicions = new JSONArray(reader.readLine());
+                    System.out.println((llistanovesPosicions));
+                } else {
+                    url = new URL(direccioServidor+"/ServicioWeb/webresources/generic/obtenerUltimaPosicion/" + matricula);
+                    reader = getBufferedReader(url);
+                    newPosicio = new JSONObject(reader.readLine());
+                }
+            }
+            catch (java.io.FileNotFoundException ex) {
+                Log.e(LOGTAG, "Error al obtenir la posicio de:" + url.toString()+"\n"+ex);
+            }catch (java.io.IOException ex) {
+                Log.e(LOGTAG, "Temps d'espera esgotat al iniciar la conexio amb la BBDD externa:" + url.toString()+"\n"+ex);
             } catch (org.json.JSONException ex) {
                 Log.e(LOGTAG, "Error en la transformacio de l'objecte JSON: " + ex);
             }
         }
+
+        /**
+         * Obte un objecte Buffered Reader que llegeix de l'objecte URL donat.
+         *
+         * @param url
+         * @return
+         * @throws java.io.IOException
+         */
+        private BufferedReader getBufferedReader(URL url) throws java.io.IOException {
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setReadTimeout(5000 /*milliseconds*/);
+            conn.setConnectTimeout(5000);
+            conn.setRequestProperty("Content-Type", "application/json");
+            return new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        }
+
+        /**
+         * Pinta una posicio al mapa. Nomes pot ser cridat des del fil de la UI, per tant, en el
+         * metode onPostExecute() de l'Async task o en el propi fil de la UI.
+         *
+         * @param posicio
+         * @throws JSONException
+         */
+        private void pintar(JSONObject posicio, int id_Color) throws JSONException {
+
+            LatLng posAutobus = new LatLng(posicio.getDouble("posX"), posicio.getDouble("posY"));
+            MarkerOptions marker = new MarkerOptions()
+                    .position(posAutobus)
+                    .snippet("Fecha posicion: "+posicio.getString("fecha"))
+                    .title(posicio.getString("matricula"))
+                    .icon(BitmapDescriptorFactory.defaultMarker(getColor(id_Color)));
+            if(firstUpdate) {
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(posAutobus, 12));
+                firstUpdate=false;
+                Log.d(LOGTAG, "Moving zoom to first position.");
+            }
+            marcadors.add(mMap.addMarker(marker));
+        }
+
+        private float getColor(int id){
+            float color;
+            switch(id%9){
+                case 1:
+                    color=HUE_ORANGE;
+                    break;
+                case 2:
+                    color=HUE_YELLOW;
+                    break;
+                case 3:
+                    color=HUE_GREEN;
+                    break;
+                case 4:
+                    color=HUE_CYAN;
+                    break;
+                case 5:
+                    color=HUE_AZURE;
+                    break;
+                case 6:
+                    color=HUE_BLUE;
+                    break;
+                case 7:
+                    color=HUE_VIOLET;
+                    break;
+                case 8:
+                    color=HUE_MAGENTA;
+                    break;
+                default:
+                    color=HUE_RED;
+                    break;
+            }
+            return color;
+        }
+
+        private void pintarLinea(JSONObject posicio, JSONObject newPosicio) throws JSONException {
+            LatLng antPosAutobus = new LatLng(posicio.getDouble("posX"), posicio.getDouble("posY"));
+            LatLng novaPosAutobus = new LatLng(newPosicio.getDouble("posX"), newPosicio.getDouble("posY"));
+            PolylineOptions lineas = new PolylineOptions()
+                    .add(antPosAutobus)
+                    .add(novaPosAutobus);
+            lineas.width(8);
+            lineas.color(Color.RED);
+            mMap.addPolyline(lineas);
+        }
+
+        private Marker buscarMarcador(String matricula) {
+            Marker item = null;
+            for (Marker marcador : (Iterable<Marker>) marcadors) {
+                item = marcador;
+                if (item.getTitle().equals(matricula)) {
+                    break;
+                }
+            }
+            return item;
+        }
+
+        private void eliminarMarcador(Marker marcadorAEliminar) {
+            if (marcadorAEliminar != null) {
+                for (Marker item : (Iterable<Marker>) marcadors) {
+                    if (item.getTitle().equals(marcadorAEliminar.getTitle())) {
+                        item.remove();
+                    }
+                }
+            }
+        }
+
+        /**
+         * Metode que s'executa sempre que l'Async Task acaba
+         *
+         * @param result
+         */
         protected void onPostExecute(Boolean result) {
             if (!result) {
                 Log.e(LOGTAG, "No se ha podido acceder a la posicion.");
             } else {
                 try {
-                    if (posicio != null) {
-                        LatLng posAutobus = new LatLng(posicio.getDouble("posX"), posicio.getDouble("posY"));
-                        mMap.addMarker(new MarkerOptions()
-                                .position(posAutobus)
-                                .title(posicio.getString("matricula")));
-                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(posAutobus, 15));
+                    switch (opcioEscollida) {
+                        case OPCIO_ULTIMA:
+                            if (matricula_Autobus.equals("Todas")) {
+                                //TODO este codigo ha estado fallando aleatoriamente y no localizo el origen.
+                                Log.d(LOGTAG, "Pintando ultimas posiciones de todos los autobuses.");
+                                for (int i = 0; i < llistanovesPosicions.length(); i++) {
+                                    JSONObject jsonobject = llistanovesPosicions.getJSONObject(i);
+                                    Marker marcador=buscarMarcador(jsonobject.getString("matricula"));
+                                    if(marcador!=null){
+                                        eliminarMarcador(marcador);
+                                    }
+                                    pintar(jsonobject, i);
+                                }
+                            } else {
+                                if (newPosicio != null) {
+                                    Log.d(LOGTAG, "Pintando ultima posicion del autobus:"+matricula_Autobus);
+                                    if (posicio != null) {
+                                        if (!newPosicio.toString().equals(posicio.toString())) {
+                                            eliminarMarcador(buscarMarcador(matricula_Autobus));
+                                            pintar(newPosicio,1);
+                                            posicio = newPosicio;
+                                        } else {
+                                            Log.d(LOGTAG, "La posicio no ha cambiat");
+                                        }
+                                    } else {
+                                        pintar(newPosicio,1);
+
+                                        posicio = newPosicio;
+                                    }
+                                } else {
+                                    Toast.makeText(MapsActivity.this, "No se ha podido acceder a la posicion del autobus:"+matricula_Autobus, Toast.LENGTH_SHORT).show();
+                                    Log.e(LOGTAG, "No se ha podido acceder a la posicion del autobus:"+matricula_Autobus);
+                                    desactivarActualitzacionsAutomatiques();
+                                }
+                            }
+                            break;
+                        case OPCIO_ENTRE_DADES:
+                            //TODO todo el codigo. (aqui va muuuucccchoooo codigo.)
+                            break;
                     }
-                    else{
-                        Log.e(LOGTAG, "Error en la obtencio de la posicio");
-                    }
-                }catch(org.json.JSONException ex){
-                        Log.e(LOGTAG, "Error en la transformacio de l'objecte JSON: " + ex);
-                    }
+                } catch (org.json.JSONException ex) {
+                    Log.e(LOGTAG, "Error en la transformacio de l'objecte JSON: " + ex);
+                }
             }
         }
-    }
 
+    }
 }
 
